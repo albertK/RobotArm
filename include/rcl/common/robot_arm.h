be045b4a8266844509dee7241d54c2ecb69cfd49
albert <knowledge80122@gmail.com>
2015-06-09 15:49:52 +0800

    for debugging

--- include/rcl/common/robot_arm.h
+++ include/rcl/common/robot_arm.h
@@ -1,10 +1,36 @@
 #ifndef ROBOTARM
 #define ROBOTARM
 
+#include "rcl/application/skill.h"
+#include "rcl/application/default_mode.h"
+#include "rcl/application/cartesian_mode.h"
+#include "rcl/application/joint_mode.h"
+
+#include "rcl/basic_utility/robot_states.h"
+#include "rcl/basic_utility/motor_joint.h"
+#include "rcl/basic_utility/kinematics.h"
+#include "rcl/basic_utility/dynamics.h"
+
+#include "rcl/common/parameters.h"
+#include "rcl/hardware/analog_output.h"
+#include "rcl/hardware/encoder.h"
+
 namespace rcl
 {
     class RobotArm
     {
+    public:
+	Skill* skill;
+	DefaultMode default_mode;
+	JointMode joint_mode;
+	
+	RobotStates robot_states;
+	MotorJoint motor_joint;
+	Kinematics kinematics;
+	Dynamics dynamics;
+	
+	AnalogOutput ao;
+	Encoder encoder;
     };
 };
 
