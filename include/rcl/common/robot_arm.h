51dfeb26d98efd688be8b561412bdefbc08dca88
albert <knowledge80122@gmail.com>
2015-06-11 17:37:27 +0800

    complete the draft of robot_arm.h

--- include/rcl/common/robot_arm.h
+++ include/rcl/common/robot_arm.h
@@ -1,10 +1,13 @@
 #ifndef ROBOTARM
 #define ROBOTARM
 
+#include <string>
+
 #include "rcl/application/skill.h"
 #include "rcl/application/default_mode.h"
 #include "rcl/application/cartesian_mode.h"
 #include "rcl/application/joint_mode.h"
+#include "rcl/application/intuive_teach.h"
 
 #include "rcl/basic_utility/robot_states.h"
 #include "rcl/basic_utility/motor_joint.h"
@@ -19,10 +22,35 @@ namespace rcl
 {
     class RobotArm
     {
+    protected:
+	std::string mode_;
+	void clock1_update();
+	void clock2_update();
+	void clock3_update();
     public:
+	//initialize the robot including all the sub modules and hardware
+	void init();
+	
+	//after the call to init(), go to the ready pose
+	void ready();
+	
+	/*
+	 * choose what application to use now
+	 * car: Cartesian Mode
+	 * def: Default Mode
+	 * int: Intuive Teach Mode
+	 * joi: Joint Mode
+	 */
+	void setApplicationMode(std::string mode);
+	
+	//release resources, do garbage collection. call when exit the program
+	void quit();
+	
 	Skill* skill;
 	DefaultMode default_mode;
 	JointMode joint_mode;
+	CartesianMode cartesian_mode;
+	IntuiveTeach intuive_teach;
 	
 	RobotStates robot_states;
 	MotorJoint motor_joint;
